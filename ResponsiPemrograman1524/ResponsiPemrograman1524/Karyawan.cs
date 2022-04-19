9e98b9bc8597fe90ebe89ad9c1bac0d459436fd8
Nur Karimah <nurkarimah12345@gmail.com>
2022-04-19 16:06:48 +0700

    Responsi Pemrograman 1524

---
+++ ResponsiPemrograman1524/ResponsiPemrograman1524/Karyawan.cs
@@ -0,0 +1,40 @@
+﻿namespace ResponsiPemrograman1524
+{
+    public class Karyawan
+    {
+
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+
+        public void infoGajiKaryawan()
+        {
+            if (GajiBulanan < 1)
+            {
+                Console.WriteLine("Gaji Bulanan Tidak Valid");
+            }
+            else
+            {
+                Console.WriteLine("No NIK/Nama              Gaji Bulanan");
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("1. {0}/{1}             Rp.{2:#,0}", Nik, Nama, GajiBulanan);
+                naikGajiKaryawan();
+            }
+        }
+
+        public void naikGajiKaryawan()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+
+            Console.WriteLine("Assyikk naik gaji 10% nich");
+            int naik = (GajiBulanan / 100) * 10;
+            int hasilAkhir = naik + GajiBulanan;
+            Console.WriteLine("No NIK/Nama              Gaji Bulanan");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("1. {0}/{1}             Rp.{2:#,0}", Nik, Nama, hasilAkhir);
+        }
+    }
+}
---
+++ ResponsiPemrograman1524/ResponsiPemrograman1524/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ResponsiPemrograman1524
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan = new Karyawan();
+            Console.WriteLine("Masukkan NIK Karyawan");
+            string Nik = Console.ReadLine();
+            karyawan.Nik = Nik;
+            Console.WriteLine("Masukkan Nama Karyawan");
+            string Nama = Console.ReadLine();
+            karyawan.Nama = Nama;
+            Console.WriteLine("Masukkan Gaji Karyawan");
+            int GajiBulanan = int.Parse(Console.ReadLine());
+            karyawan.GajiBulanan = GajiBulanan;
+
+
+            karyawan.infoGajiKaryawan();
+        }
+    }
+}
