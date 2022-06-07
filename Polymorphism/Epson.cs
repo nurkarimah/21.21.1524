cd9017835fc4a1ea5295adb501ef4e620d3a3e2a
Nur Karimah <nurkarimah12345@gmail.com>
2022-06-07 20:05:05 +0700

    Polymorphism

---
+++ Polymorphism/Epson.cs
@@ -0,0 +1,15 @@
+﻿namespace Polymorphism
+{
+    internal class Epson : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Epson display dimension : 10*11");
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Epson printing....");
+        }
+    }
+}
